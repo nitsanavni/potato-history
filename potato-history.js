fa60a7844267d267f4c1488549fc6925f53bd589
Nitsan Avni <nitsan@papayaglobal.com>
2020-01-31 14:06:23 +0700

    wip

---
+++ potato-history.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const _ = require("lodash");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Search } = require("potato-search");
+const chalk = require("chalk");
